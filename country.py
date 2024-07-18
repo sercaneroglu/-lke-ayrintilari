e481d332f90253f0eceb387a9a6d37be22e170fa
root@voxendev <152682533+sercaneroglu@users.noreply.github.com>
2024-07-18 22:26:37 +0300

    Add files via upload

---
+++ country.py
@@ -0,0 +1,16 @@
+from countryinfo import CountryInfo
+
+country CountryInfo (input("Enter Country Name:"))
+print("Capital:", country.capital())
+print("Population:", country.population())
+print("Area (in square kilometers):", country.area())
+print("Region:", country.region())
+print("Subregion:", country.subregion())
+print("Demonym:", country.demonym())
+print("Currency:", country.currencies())
+print("Languages:", country.languages())
+print("Borders: country.borders())
+
+# python.org
+# github.com/sercaneroglu
+# İllegal Kullanmak Yasak!
